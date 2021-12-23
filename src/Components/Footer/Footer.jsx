3aa0fe9b8e7f50623f70cab56e11d5a854c26c20
Jaafar Yassine <jaafaryassine.27@gmail.com>
2021-12-23 23:43:12 +0100

    v1.0

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Instagram,Facebook,LinkedIn,GitHub} from "@material-ui/icons"
+import './footer.scss'
+
+export default function Footer() {
+    return (
+        <div>
+            <footer class="footer-distributed">
+
+<div class="footer-right">
+
+  <a href="https://www.instagram.com/jfryassine/" target="_blank" rel="noopener noreferrer"><Instagram className="icon"/></a>
+  <a href="https://www.linkedin.com/in/jaafaryassine/" target="_blank" rel="noopener noreferrer"><LinkedIn className="icon"/></a>
+  <a href="https://github.com/jfryassine" target="_blank" rel="noopener noreferrer"><GitHub className="icon"/></a>
+
+</div>
+
+<div class="footer-left">
+
+  <p class="footer-links">
+    <a class="link-1" href="#">jy.</a>
+  </p>
+
+  <p>Jaafar Yassine &copy; 2021</p>
+</div>
+
+</footer>
+        </div>
+    )
+}
